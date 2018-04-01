db431583eb8ec8564cfec424bccd966b6b1a8bb4
Gabriel Majeri <gabriel.majeri6@gmail.com>
2018-04-01 08:09:40 +0300

    Add Team Code module

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NullOpMode.kt
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.util.ElapsedTime
+
+@Autonomous(name = "Null OpMode", group = "Tests")
+class NullOpMode: OpMode() {
+    val timer = ElapsedTime()
+
+    override fun init() {
+        telemetry.addData("Status", "Null OpMode is initialized")
+        telemetry.update()
+    }
+
+    override fun start() {
+        timer.reset()
+    }
+
+    override fun loop() {
+        telemetry.addData("Status", "Null OpMode is running, not doing anything")
+        telemetry.addData("Time", "%d", timer.seconds())
+        telemetry.update()
+    }
+}
