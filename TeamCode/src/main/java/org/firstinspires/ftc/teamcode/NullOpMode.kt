02f3435f3b103b4588e00f6d0e29334dc25da316
Matei Chesa <chesamatei@yahoo.ro>
2018-04-02 15:14:47 +0300

    Add telemetry delay

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NullOpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NullOpMode.kt
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.util.ElapsedTime
 @Autonomous(name = "Null OpMode", group = "Tests")
 class NullOpMode: OpMode() {
     private val timer = ElapsedTime()
+    private var lastTime = timer.milliseconds()
 
     override fun init() {
         telemetry.addData("Status", "Null OpMode is initialized")
@@ -18,8 +19,12 @@ class NullOpMode: OpMode() {
     }
 
     override fun loop() {
-        telemetry.addData("Status", "Null OpMode is running, not doing anything")
-        telemetry.addData("Time", "%d", timer.seconds())
-        telemetry.update()
+        if (timer.milliseconds() - lastTime > 250) {
+            telemetry.addData("Status", "Null OpMode is running, not doing anything")
+            telemetry.addData("Time", "%f", timer.seconds())
+            telemetry.update()
+
+            lastTime = timer.milliseconds()
+        }
     }
 }
