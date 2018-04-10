2001dfeabe13ed4e2d31d56e88317c9fbffa7c7f
Matei Chesa <chesamatei@yahoo.ro>
2018-04-10 18:07:41 +0300

    Update test code

--- QubeCode/src/main/cpp/vl53l0x/VL53L0X.cpp
+++ QubeCode/src/main/cpp/vl53l0x/VL53L0X.cpp
@@ -1,11 +1,7 @@
 #include "VL53L0X.hpp"
 
-#include <iostream>
-#include <string>
-#include <vector>
-
-using namespace std;
-
 jstring NATIVE_FUNC(stringFromJNI)(JNIEnv *env, jobject self) {
+
+
     return env->NewStringUTF("Hello");
 }
