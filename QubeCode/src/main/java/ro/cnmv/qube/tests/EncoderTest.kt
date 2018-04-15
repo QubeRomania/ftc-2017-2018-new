c7a26bd36f273983b30d1b689526f79bb53bc68e
andreicod1999 <andrei.daha@cnmv.ro>
2018-04-15 14:27:26 +0300

    Test constant velocity PID

--- QubeCode/src/main/java/ro/cnmv/qube/tests/EncoderTest.kt
+++ QubeCode/src/main/java/ro/cnmv/qube/tests/EncoderTest.kt
@@ -9,13 +9,9 @@ class EncoderTest: AutonomousOpMode() {
     override fun Hardware.run() {
         motors.resetPosition()
 
-        var aLastState = gamepad1.a
         var bLastState = gamepad1.b
 
         while (opModeIsActive()) {
-            if (gamepad1.a != aLastState && gamepad1.a)
-                motors.runWithConstantVelocity()
-
             if (gamepad1.b != bLastState && gamepad1.b)
                 motors.resetPosition()
 
@@ -24,7 +20,6 @@ class EncoderTest: AutonomousOpMode() {
             motors.printPosition(telemetry)
             telemetry.update()
 
-            aLastState = gamepad1.a
             bLastState = gamepad1.b
         }
     }
---
+++ QubeCode/src/main/java/ro/cnmv/qube/tests/VelocityPIDTest.kt
@@ -0,0 +1,25 @@
+package ro.cnmv.qube.tests
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.util.ElapsedTime
+import ro.cnmv.qube.AutonomousOpMode
+import ro.cnmv.qube.hardware.Hardware
+
+@Autonomous(name = "Velocity PID Test", group = "Tests")
+class VelocityPIDTest: AutonomousOpMode() {
+    override fun Hardware.run() {
+        motors.resetPosition()
+
+        waitForStart()
+
+        motors.runWithConstantVelocity()
+        motors.translate(0.0, 0.5)
+
+        val timer = ElapsedTime()
+
+        while (opModeIsActive() && timer.milliseconds() <= 10000) {
+            motors.printTelemetry(telemetry)
+            telemetry.update()
+        }
+    }
+}
