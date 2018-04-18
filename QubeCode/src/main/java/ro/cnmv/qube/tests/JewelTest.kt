0d7c48ba9f699bb1fe4a6cc17bef03924c7234bc
Matei Chesa <chesamatei@yahoo.ro>
2018-04-18 15:40:18 +0300

    Add jewel arm test

---
+++ QubeCode/src/main/java/ro/cnmv/qube/tests/JewelTest.kt
@@ -0,0 +1,15 @@
+package ro.cnmv.qube.tests
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import ro.cnmv.qube.systems.Jewel
+
+@Autonomous(name = "Jewel Hit Test", group = "Tests")
+class JewelTest: LinearOpMode() {
+    override fun runOpMode() {
+        val jewel = Jewel(hardwareMap, this)
+
+        // Our color is RED, hit BLUE.
+        jewel.hitJewel(Jewel.Color.RED)
+    }
+}
