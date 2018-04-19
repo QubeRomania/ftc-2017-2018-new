71c40fc30f337fcce2458eeee4f9c53d8c982f7e
andreicod1999 <andrei.daha@cnmv.ro>
2018-04-19 03:34:41 +0300

    Changed to being able to handle translation at any angle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/RangePIDTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/RangePIDTest.kt
@@ -4,7 +4,12 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
 import ro.cnmv.qube.OpMode
 import ro.cnmv.qube.hardware.Hardware
+import kotlin.math.atan2
+import kotlin.math.sqrt
 
+/*
+ FAR BLUE:
+ */
 @Autonomous(name = "Range PID Test", group = "Tests")
 class RangePIDTest: OpMode() {
     override fun Hardware.run() {
@@ -13,14 +18,27 @@ class RangePIDTest: OpMode() {
         gyro.enableTelemetry(telemetry)
 
         while (opModeIsActive()) {
-            val headingCorrection = getHeadingCorrection(0.0)
-            val distanceCorrection = backDistance.getDistanceCorrection(50.0)
+            val targetRightDistance = 45.0
+            val targetBackDistance = 45.0
 
-            motors.move(0.0, distanceCorrection, headingCorrection)
+            val headingCorrection = getHeadingCorrection(0.0)
+            val rightDistanceCorrection = rightDistance.getDistanceCorrection(targetRightDistance)
+            val backDistanceCorrection = backDistance.getDistanceCorrection(targetBackDistance)
+            val moveHeading = Math.atan2(-(rightDistance.getDistance(DistanceUnit.CM) - targetRightDistance), -(backDistance.getDistance(DistanceUnit.CM) - targetBackDistance)) * 180 / Math.PI
+            var speed = sqrt((backDistanceCorrection * backDistanceCorrection) + (rightDistanceCorrection * rightDistanceCorrection))
+
+            speed = Math.min(speed, 0.8)
+            if (speed < 0.1 && speed > 0.01)
+                speed = 0.1
+
+            motors.move(moveHeading, speed, headingCorrection)
 
             telemetry.addData("Heading Correction", "%.3f", headingCorrection)
-            telemetry.addData("Distance Correction", "%.3f", distanceCorrection)
-            telemetry.addData("Distance", "%.2f mm", backDistance.getDistance(DistanceUnit.CM))
+            telemetry.addData("Direction", "%.3f", moveHeading)
+            telemetry.addData("Back Distance Correction", "%.3f", backDistanceCorrection)
+            telemetry.addData("Right Distance Correction", "%.3f", rightDistanceCorrection)
+            telemetry.addData("Back Distance", "%.2f cm", backDistance.getDistance(DistanceUnit.CM))
+            telemetry.addData("Right Distance", "%.2f cm", rightDistance.getDistance(DistanceUnit.CM))
 
             telemetry.update()
         }
