87c317e9ca2592560c5965a5176dec2e986a97b7
andreicod1999 <andrei.daha@cnmv.ro>
2018-04-19 03:28:40 +0300

    Update PID

--- QubeCode/src/main/java/ro/cnmv/qube/OpMode.kt
+++ QubeCode/src/main/java/ro/cnmv/qube/OpMode.kt
@@ -1,11 +1,14 @@
 package ro.cnmv.qube
 
+import android.support.annotation.MainThread
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.hardware.PIDCoefficients
 import com.qualcomm.robotcore.util.ElapsedTime
+import com.qualcomm.robotcore.util.Range
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
 import ro.cnmv.qube.hardware.Hardware
+import kotlin.math.absoluteValue
 
 abstract class OpMode: LinearOpMode() {
     private val hw by lazy {
@@ -28,7 +31,7 @@ abstract class OpMode: LinearOpMode() {
     private var lastRotationError = 0.0
 
     fun getHeadingCorrection(targetHeading: Double): Double {
-        val pid = PIDCoefficients(0.8, 0.0, 1.5)
+        val pid = PIDCoefficients(1.6, 0.2, 0.4)
 
         // Determine the rotation error.
         val error = (targetHeading - hw.gyro.heading) / 90.0
@@ -40,23 +43,34 @@ abstract class OpMode: LinearOpMode() {
 
         lastRotationError = error
 
-        return correction
+        return Range.clip(correction, -1.0, 1.0)
     }
 
-    private var lastDistanceError = 0.0
+    private var lastDistanceError = HashMap<ModernRoboticsI2cRangeSensor, Double>(3)
 
     fun ModernRoboticsI2cRangeSensor.getDistanceCorrection(targetDistance: Double): Double {
+        if (!lastDistanceError.containsKey(this))
+            lastDistanceError[this] = 0.0
+
         val pid = PIDCoefficients(1.0, 0.0, 0.0)
 
-        val error = (getDistance(DistanceUnit.CM) - targetDistance) / 200.0
+        var distance = getDistance(DistanceUnit.CM)
+
+        if (distance.isNaN())
+            return 0.0
+
+        if (distance > 3000)
+            distance = 600.0
+
+        val error = (targetDistance - distance) / 100.0
 
         val correction = (pid.p * error)
-            + (pid.i * (error + lastDistanceError))
-            + (pid.d * (error - lastDistanceError))
+            + (pid.i * (error + lastDistanceError[this]!!))
+            + (pid.d * (error - lastDistanceError[this]!!))
 
-        lastDistanceError = error
+        lastDistanceError[this] = error
 
-        return correction
+        return Range.clip(correction, -1.0, 1.0)
     }
 }
 
