450b6c9a20a3d9892b15f03b957a41d9e8069ad3
andreicod1999 <andrei.daha@cnmv.ro>
2018-04-19 03:30:44 +0300

    Update servo positions and open function

--- QubeCode/src/main/java/ro/cnmv/qube/systems/RelicArm.kt
+++ QubeCode/src/main/java/ro/cnmv/qube/systems/RelicArm.kt
@@ -1,33 +1,53 @@
 package ro.cnmv.qube.systems
 
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.DcMotor
+import ro.cnmv.qube.Gamepad
 import com.qualcomm.robotcore.hardware.HardwareMap
 import org.firstinspires.ftc.robotcore.external.Telemetry
 
 class RelicArm(hwMap: HardwareMap) {
     private val armMotor = hwMap.dcMotor["arm_motor"]
+    private val armLiftServo = hwMap.servo["arm_lift_servo"]
+    private val rlicGrabServo = hwMap.servo["relic_grab_servo"]
 
     companion object {
-        private const val openPosition = -930
+        private const val openPosition = 930
         private const val closedPosition = 0
+        private const val armOpenPosition = 1.0
+        private const val armClosedPosition = 0.0
+        private const val relicOpenPosition = 1.0
+        private const val relicClosedPosition = 0.0
     }
 
     init {
+        armMotor.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
         armMotor.mode = DcMotor.RunMode.RUN_USING_ENCODER
         armMotor.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
         armMotor.targetPosition = closedPosition
         armMotor.power = 0.15
+
+        armLiftServo.position = armClosedPosition
     }
 
-    fun open(open: Boolean) {
-        if(open) {
-            armMotor.targetPosition = openPosition
-        } else {
-            armMotor.targetPosition = closedPosition
-        }
+    private var extendPosition = 0
+
+    fun open(open: Double) {
+        extendPosition += (open * 10).toInt()
+
+        if (extendPosition > openPosition)
+            extendPosition = openPosition
+        if (extendPosition < closedPosition)
+            extendPosition = closedPosition
+
+        armMotor.targetPosition = extendPosition
 
         armMotor.mode = DcMotor.RunMode.RUN_TO_POSITION
+    }
+
+    private var heghtPosition = 0.0
+
+    fun setHeight(position: Double) {
+
     }
 
     fun printTelemetry(telemetry: Telemetry) {
